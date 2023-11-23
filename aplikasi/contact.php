27223d1f0e0ccb706e5fb6ce3b7ed1fb3aa362c4
Muh. Falah Mubaraq <120860186+lalalafalah@users.noreply.github.com>
2023-11-24 06:11:13 +0700

    Update contact.php

--- aplikasi/contact.php
+++ aplikasi/contact.php
@@ -58,8 +58,10 @@
 
     <!-- ======= Testimonials Section ======= -->
     <section id="testimonials" class="testimonials section-bg" style="background: #3b5998; ">
-      <div class="container" data-aos="fade-up">
-        <div class="card">
+    <div class="container">
+      <div class="card">
+        <div class="container" data-aos="fade-up">
+
 
           <div class="section-header mt-5">
 
@@ -75,7 +77,7 @@
 
               while ($data = mysqli_fetch_array($sql)) {
 
-                ?>
+              ?>
 
                 <div class="swiper-slide">
                   <div class="testimonial-item">
@@ -100,28 +102,28 @@
                             ?>
                           </h4>
                           <div class="stars">
-                            <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i
-                              class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
+                            <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
                           </div>
                         </div>
                       </div>
                       <div class="col-lg-2 text-center">
                         <img src="gambar/anggota/<?php
-                        echo $data['foto'];
-                        ?>" class="img-fluid testimonial-img" alt="">
+                                                  echo $data['foto'];
+                                                  ?>" class="img-fluid testimonial-img" alt="">
                       </div>
                     </div>
                   </div>
                 </div>
-
               <?php } ?>
+
             </div>
             <div class="swiper-pagination"></div>
           </div>
 
         </div>
       </div>
-    </section>
+    </div>
+  </section>
 
     <!-- ======= Events Section ======= -->
 
