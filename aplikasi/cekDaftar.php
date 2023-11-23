8980581810feef8f0d87aeccfcf9c2f29431a88b
Muh. Falah Mubaraq <120860186+lalalafalah@users.noreply.github.com>
2023-11-24 01:05:27 +0700

    Menambahkan Konsep OOP

--- aplikasi/cekDaftar.php
+++ aplikasi/cekDaftar.php
@@ -1,14 +1,42 @@
 <?php
 
-class UserRegistration {
-    private $konek;
+class Database {
+    private $host = 'localhost';
+    private $username = 'root';
+    private $password = '';
+    private $database = 'db_publik';
+    protected $connection;
 
-    public function __construct($databaseConnection) {
-        $this->konek = $databaseConnection;
+    // Konstruktor untuk membuat koneksi ke database
+    public function __construct() {
+        $this->connect();
     }
 
+    // Fungsi untuk membuat koneksi ke database
+    protected function connect() {
+        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+
+        // Periksa apakah koneksi sukses atau tidak
+        if ($this->connection->connect_error) {
+            die('Koneksi database gagal: ' . $this->connection->connect_error);
+        }
+    }
+
+    // Fungsi untuk menutup koneksi ke database
+    public function closeConnection() {
+        if ($this->connection) {
+            $this->connection->close();
+        }
+    }
+
+    // Fungsi lain yang mungkin dibutuhkan dalam aplikasi Anda
+    // ...
+
+}
+
+class UserRegistration extends Database {
     public function registerUser($nik, $namalengkap, $email, $no_telepon, $password) {
-        $query = mysqli_query($this->konek, "INSERT INTO tb_user VALUES('$nik','$namalengkap','$email','$no_telepon', '$password')");
+        $query = mysqli_query($this->connection, "INSERT INTO tb_user VALUES('$nik','$namalengkap','$email','$no_telepon', '$password')");
 
         if ($query) {
             $this->redirectToLogin();
@@ -27,8 +55,6 @@ class UserRegistration {
 }
 
 // Example usage:
-include 'konek.php';
-
 if (isset($_POST['btnDaftar'])) {
     $nik = $_POST['nik'];
     $namalengkap = $_POST['namalengkap'];
@@ -36,7 +62,10 @@ if (isset($_POST['btnDaftar'])) {
     $no_telepon = $_POST['no_telepon'];
     $password = $_POST['password'];
 
-    $userRegistration = new UserRegistration($konek);
+    $userRegistration = new UserRegistration();
     $userRegistration->registerUser($nik, $namalengkap, $email, $no_telepon, $password);
+
+    // Menutup koneksi setelah selesai menggunakan database
+   
 }
 ?>
