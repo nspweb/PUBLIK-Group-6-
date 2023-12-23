1786175adde5433e48d4ea61ca3137f161f48c5d
lalalafalah <muhfalahmubaraq@gmail.com>
2023-12-23 08:45:25 +0700

    meambahkan keterangan pada cekLoginOOP.php

--- aplikasi/cekLoginOOP.php
+++ aplikasi/cekLoginOOP.php
@@ -1,23 +1,23 @@
 <?php
-
-class Database
+#E1E122020_MUH. FALAH MUBARAQ
+class Database //Membuat class Database yang nantinya akan menjadi class parent/kelas induk.
 {
-    private $host = "localhost", $user = "root", $pass = "", $db = "db_publik", $conn;
+    private $host = "localhost", $user = "root", $pass = "", $db = "db_publik", $conn; //tambhakan properti dengan visibility di awal sebelum properti
 
-    public function __construct()
+    public function __construct() //membuat konstruktornya yang merupakan magic metod pada php
     {
-        $this->conn = mysqli_connect($this->host, $this->user, $this->pass, $this->db);
+        $this->conn = mysqli_connect($this->host, $this->user, $this->pass, $this->db); //this->untuk menyamakan properti di luar function,karena function 
     }
-    public function getKonek()
+    public function getKonek()//membuat metod getKonek yang akan mengembalikan nilai $con
     {
         return $this->conn;
     }
 }
-class userLogin extends Database
+class userLogin extends Database //membuat child class/kelas anak yaitu userLogin
 {
     public function cekLogin($nik, $password)
     {
-        $konek = parent::getKonek();
+        $konek = parent::getKonek(); 
         $query = mysqli_query($konek, "SELECT * FROM tb_user WHERE nik='$nik'");
         $data = mysqli_fetch_array($query);
         if (mysqli_num_rows($query) >= 1) {
