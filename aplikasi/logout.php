1ac70348742debaee8b3bf71302a4c396992c4f2
annisanurfadilah018@gmail.com <annisanurfadilah018@gmail.com>
2023-12-27 12:37:31 +0800

    tambah komen di logout

--- aplikasi/logout.php
+++ aplikasi/logout.php
@@ -1,6 +1,13 @@
 <?php
+// Memulai sesi untuk mengakses atau menghapus data sesi
 session_start();
+
+// Menghapus semua variabel sesi
 session_unset();
+
+// Menghancurkan atau mengakhiri sesi
 session_destroy();
+
+// Mengarahkan pengguna kembali ke halaman login setelah sesi dihapus
 header('location:login.php');
 ?>
