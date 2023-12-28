3e55a778e9c480ba650f8fac0036790d1cbf9371
annisanurfadilah018@gmail.com <annisanurfadilah018@gmail.com>
2023-12-28 20:59:08 +0800

    tambahan

--- aplikasi/cekDaftar.php
+++ aplikasi/cekDaftar.php
@@ -1,44 +1,59 @@
 <?php
 
+// Include file untuk koneksi database
 include 'konek.php';
-class userDaftar extends Database
+
+// Interface Registrasi
+// Interface menyediakan kerangka atau kontrak yang harus dipatuhi oleh kelas-kelas yang mengimplementasikannya.
+interface Registrasi {
+    public function cekDaftar($nik, $namalengkap, $email, $no_telepon, $password);
+}
+
+// Kelas userDaftar yang meng-extend Database dan meng-implement Registrasi
+class userDaftar extends Database implements Registrasi
 {
     public function __construct()
     {
         parent::__construct();
     }
 
-    public function cekDaftar($nik,$namalengkap, $email,$no_telepon,$password)
+    // Implementasi method cekDaftar dari interface Registrasi
+    public function cekDaftar($nik, $namalengkap, $email, $no_telepon, $password)
     {
         $konek = $this->getKonek();
         $query = mysqli_query($konek, "INSERT INTO tb_user VALUES('$nik','$namalengkap','$email','$no_telepon', '$password')");
         return $query;
     }
 }
+
+// Instance objek dari kelas userDaftar
 $daftar = new userDaftar();
 
-
+// Proses registrasi saat tombol btnDaftar diklik
 if (isset($_POST['btnDaftar'])) {
+    // Ambil data dari form
     $nik = $_POST['nik'];
     $namalengkap = $_POST['namalengkap'];
     $email = $_POST['email'];
     $no_telepon = $_POST['no_telepon'];
     $password = $_POST['password'];
 
-    // Register user
-    $sql = $daftar->cekDaftar($nik,$namalengkap, $email,$no_telepon,$password);
+    // Panggil method cekDaftar untuk melakukan registrasi
+    $sql = $daftar->cekDaftar($nik, $namalengkap, $email, $no_telepon, $password);
 
+    // Cek apakah registrasi berhasil
     if ($sql) {
-        // Registration successful
+        // Registrasi sukses
         echo "
         <script>
         alert('User registration successful!');
         window.location.href='login.php';
         </script>";
     } else {
-        // Registration failed
+        // Registrasi gagal
         echo "Registration failed. Please try again.";
     }
 }
 
+//  konsep polimorfisme terlihat dari kemampuan untuk menggunakan objek dari kelas yang berbeda (dalam contoh proyek ini adalah kelas userDaftar) melalui interface (Registrasi). Ini memungkinkan pemanggilan metode dengan nama yang sama (cekDaftar) pada objek berbagai kelas yang mengimplementasikan interface tersebut.
 ?>
